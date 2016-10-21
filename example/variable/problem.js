c868205355bb67897c4d49f2ba73f24724cd0834
teramotodaiki <i101323@gmail.com>
2016-10-21 13:34:36 +0900

    problem: variable

---
+++ example/variable/problem.js
@@ -0,0 +1,20 @@
+var a = 1 ; a == 1
+var a = N ; a == i
+var a = N ; a + N == i
+var a = N ; i - a == N
+var a = N + N ; a == i
+var a = N - N ; a == i
+var a = 2 ; var b = 5 ; a + b == 7
+var a = N ; var b = N ; N - a - b == i
+var a = N ; var b = N ; N - a + b == i
+var a = 1 ; a = a + 1 ; a == 2
+var a = N ; a = a + N ; a == i
+var a = N ; a = a - N ; a == i
+var a = N ; a = N - a ; a + N == i
+var a = N ; a = N - a ; N - a == i
+var a = N ; a = N + i ; N + a == N
+var a = N ; var b = N ; a + b == i
+var a = N ; var b = i ; N - a + b == N
+var a = N ; var b = a + N ; b == i
+var a = i ; var b = N - a ; b == N
+var a = N ; var b = a - i ; b == N
